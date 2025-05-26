3be26cb7ad2325ed5812c09222d0c9558a392bc4
Ennxin <janethoo210103@gmail.com>
2025-05-26 16:51:18 +0800

    Add files via upload

---
+++ adminindex.php
@@ -0,0 +1,12 @@
+<?php include('adminheader.php'); ?>
+<link rel="stylesheet" href="styles/admin.css">
+
+<div class="admin-container">
+    <a href="feedback.php" class="admin-card">Users' Feedback</a>
+    <a href="talents.php" class="admin-card">Talent Portfolios</a>
+    <a href="manageusers.php" class="admin-card">Manage Users</a>
+    <a href="announcements.php" class="admin-card">Announcement Management</a>
+    <a href="faq.php" class="admin-card">FAQ Management</a>
+</div>
+
+<?php include 'footer.php'; ?>
