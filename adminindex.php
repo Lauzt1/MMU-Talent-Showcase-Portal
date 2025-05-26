3062d44687a854b1fa0cf5925c05f68a3f9d9f94
Ennxin <janethoo210103@gmail.com>
2025-05-26 18:37:24 +0800

    Update adminindex.php

--- adminindex.php
+++ adminindex.php
@@ -12,7 +12,7 @@
     <a href="feedback.php" class="admin-card">Users' Feedback</a>
     <a href="talents.php" class="admin-card">Talent Portfolios</a>
     <a href="manageusers.php" class="admin-card">Manage Users</a>
-    <a href="announcements.php" class="admin-card">Announcement Management</a>
+    <a href="announcement.php" class="admin-card">Announcement Management</a>
     <a href="faq.php" class="admin-card">FAQ Management</a>
 </div>
 
