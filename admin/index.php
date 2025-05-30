ba37f1671ff5b440093939f1cb99a3b1e408413e
Ennxin <janethoo210103@gmail.com>
2025-05-30 19:29:44 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -9,9 +9,9 @@
     </section>
       
 <div class="admin-container">
-    <a href="feedback.php" class="admin-card">Users' Feedback</a>
-    <a href="talents.php" class="admin-card">Talent Portfolios</a>
-    <a href="manageusers.php" class="admin-card">Manage Users</a>
+    <a href="feedback.php" class="admin-card">Check Users' Feedback</a>
+    <a href="talents.php" class="admin-card">Talent Portfolios Approval</a>
+    <a href="manageusers.php" class="admin-card">Manage User's Profile</a>
     <a href="announcement.php" class="admin-card">Announcement Management</a>
     <a href="faq-admin.php" class="admin-card">FAQ Management</a>
 </div>
