460a63b25f1fa74f75202248575622d7138a067b
xinyuennnn <angelimxy@gmail.com>
2025-06-20 10:28:15 +0800

    Updated index.php

--- admin/index.php
+++ admin/index.php
@@ -15,5 +15,6 @@
     <a href="announcement.php" class="admin-card">Announcement Management</a>
     <a href="faq-admin.php" class="admin-card">FAQ Management</a>
     <a href="approve_passwords.php" class="admin-card">User's Password Request</a></div>
+    <a href="approve_passwords.php" class="admin-card">User's Password Request</a>
 
 <?php include 'footer.php'; ?>
